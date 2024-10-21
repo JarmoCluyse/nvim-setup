a2f5e0906d4039080d6347ed9faa412b7223f398
Jarmo Cluyse <jarmo.cluyse@movu-robotics.com>
2024-10-21 09:08:01 +0200

    feat: added keymap to replace word

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -1,16 +1,18 @@
 -- NOTE: [[ Basic Keymaps ]]
 
 -- cspell:ignore nohlsearch setloclist uickfix
+local keymap = vim.keymap
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("v", "<K>", "<cmd>:m -2<CR>gv=gv", { desc = "Move line up by one" })
-vim.keymap.set("v", "<J>", "<cmd>:m +1<CR>gv=gv", { desc = "Move line down by one" })
-vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste and void yank" })
-vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete to void" })
-vim.keymap.set("v", "<leader>d", '"_d', { desc = "Delete to void" })
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+keymap.set("v", "<K>", "<cmd>:m -2<CR>gv=gv", { desc = "Move line up by one" })
+keymap.set("v", "<J>", "<cmd>:m +1<CR>gv=gv", { desc = "Move line down by one" })
+keymap.set("x", "<leader>p", '"_dP', { desc = "Paste and void yank" })
+keymap.set("n", "<leader>d", '"_d', { desc = "Delete to void" })
+keymap.set("v", "<leader>d", '"_d', { desc = "Delete to void" })
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
