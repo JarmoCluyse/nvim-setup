0f1b47150ca13567f581fb8335c663c21193630b
Jarmo Cluyse <jarmo.cluyse@movu-robotics.com>
2024-11-27 16:26:02 +0100

    feat: added csharp debugger

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -36,26 +36,37 @@ return { -- INFO: Debugging and Profiling
     local dap = require("dap")
     local dapui = require("dapui")
 
+    vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+    vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+    vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+    vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+    vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" })
+    vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
     require("mason-nvim-dap").setup({
       automatic_installation = true,
       handlers = {},
       ensure_installed = {
         "delve", -- GO debugger
+        "coreclr", -- dotnet debugger
       },
     })
     dapui.setup({
-      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "" },
       controls = {
         icons = {
-          pause = "⏸",
-          play = "▶",
-          step_into = "⏎",
-          step_over = "⏭",
-          step_out = "⏮",
-          step_back = "b",
-          run_last = "▶▶",
-          terminate = "⏹",
-          disconnect = "⏏",
+          pause = "",
+          play = "",
+          step_into = "",
+          step_over = "",
+          step_out = "",
+          step_back = "",
+          run_last = "",
+          terminate = "",
+          disconnect = "",
         },
       },
     })
--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,4 +1,6 @@
--- NOTE: [[ Neotest ]] is a testing framework for [[ Neovim ]] that supports multiple test runners.
+--  NOTE: [[ Neotest ]] is a testing framework for [[ Neovim ]] that supports multiple test runners.
+
+---@diagnostic disable: missing-fields
 
 return {
   {
@@ -9,6 +11,7 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "marilari88/neotest-vitest",
+      "Issafalcon/neotest-dotnet",
     },
     config = function()
       require("neotest").setup({
@@ -17,13 +20,34 @@ return {
 
             vitestCommand = "npx vitest run",
           }),
+          require("neotest-dotnet")({
+            dap = {
+              args = { justMyCode = false },
+              adapter_name = "coreclr",
+              discovery_root = "solution", -- or "solution (.sln)" or "project (.csproj)"
+            },
+          }),
         },
       })
-      vim.keymap.set("n", "<leader>tc", require("neotest").run.run, { desc = "Run [T]est at [C]ursor" })
 
-      vim.keymap.set("n", "<leader>tf", function()
+      local function test_file()
         require("neotest").run.run(vim.fn.expand("%"))
-      end, { desc = "Run [T]est at [F]ile" })
+      end
+
+      local function debug_cursor()
+        require("neotest").run.run({ strategy = "dap" })
+      end
+
+      local function debug_file()
+        require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
+      end
+
+      vim.keymap.set("n", "<leader>tc", require("neotest").run.run, { desc = "Run [T]est at [C]ursor" })
+      vim.keymap.set("n", "<leader>tf", test_file, { desc = "Run [T]est at [F]ile" })
+      vim.keymap.set("n", "<leader>tC", debug_cursor, { desc = "debug [T]est at [C]ursor" })
+      vim.keymap.set("n", "<leader>tF", debug_file, { desc = "test [T]est at [F]ile" })
+
+      vim.keymap.set("n", "<leader>te", require("neotest").summary.toggle, { desc = "explore [T]ests" })
     end,
   },
 }
