32fb58d7b99abfed7416746a22b39ae2e2f61340
Jarmo Cluyse <jarmo.cluyse@movu-robotics.com>
2024-12-02 16:19:23 +0100

    feat: added resize keymaps

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -6,14 +6,24 @@ local keymap = vim.keymap
 keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
 keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+keymap.set("n", "<C-M-k>", "<cmd>:resize +2<CR>", { desc = "Resize window up" })
+keymap.set("n", "<C-M-j>", "<cmd>:resize -2<CR>", { desc = "Resize window down" })
+keymap.set("n", "<C-M-h>", "<cmd>:vertical resize +2<CR>", { desc = "Resize window left" })
+-- right needs to be remapped to avoid conflict with the ø key
+keymap.set("n", "<C-M-l>", "<cmd>:vertical resize -2<CR>", { desc = "Resize window right" })
+keymap.set("n", "ø", "<cmd>:vertical resize -2<CR>", { desc = "Resize window right" })
+
 keymap.set("n", "<M-k>", "<cmd>:m -2<CR>", { desc = "Move line up by one" })
 keymap.set("n", "<M-j>", "<cmd>:m +1<CR>", { desc = "Move line down by one" })
 keymap.set("v", "<M-k>", "<cmd>:m -2<CR>gv=gv", { desc = "Move line up by one" })
 keymap.set("v", "<M-j>", "<cmd>:m +1<CR>gv=gv", { desc = "Move line down by one" })
+
 keymap.set("x", "<leader>p", '"_dP', { desc = "Paste and void yank" })
 keymap.set("n", "<leader>d", '"_d', { desc = "Delete to void" })
 keymap.set("v", "<leader>d", '"_d', { desc = "Delete to void" })
