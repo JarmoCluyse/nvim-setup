87b62070473de5a2360d4ea762532027611ec7a8
Jarmo Cluyse <jarmo.cluyse@movu-robotics.com>
2024-12-05 14:52:59 +0100

    feat: updated prompts for copilotchat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,22 @@
 -- NOTE: [[ Copilot ]]
+local prompts = {
+  -- Code related prompts
+  Explain = "Please explain how the following code works.",
+  Review = "Please review the following code and provide suggestions for improvement.",
+  Tests = "Please explain how the selected code works, then generate unit tests for it.",
+  Refactor = "Please refactor the following code to improve its clarity and readability.",
+  FixCode = "Please fix the following code to make it work as intended.",
+  FixError = "Please explain the error in the following text and provide a solution.",
+  BetterNamings = "Please provide better names for the following variables and functions.",
+  Documentation = "Please provide documentation for the following code.",
+  SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
+  SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.",
+  -- Text related prompts
+  Summarize = "Please summarize the following text.",
+  Spelling = "Please correct any grammar and spelling errors in the following text.",
+  Wording = "Please improve the grammar and wording of the following text.",
+  Concise = "Please rewrite the following text to make it more concise.",
+}
 
 return {
   { -- INFO: Copilot Autocomplete
@@ -24,7 +42,7 @@ return {
   },
   { -- INFO: Copilot Chat
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
@@ -119,15 +137,12 @@ return {
       -- Copilot Chat Models
       { "<leader>a?", "<cmd>CopilotChatModels<cr>", desc = "CopilotChat - Select Models" },
     },
-    -- build = "make tiktoken",
-    -- See Commands section for default commands if you want to lazy load on them
     config = function()
       require("CopilotChat").setup({
-        -- debug = true, -- Enable debugging
+        prompts = prompts,
         allow_insecure = true, -- Allow insecure connection_failure_errmsg
         chat_autocomplete = true,
       })
-      -- require("CopilotChat.integrations.cmp").setup()
     end,
   },
 }
