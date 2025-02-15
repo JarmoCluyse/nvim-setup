dddd296042de01293af7be433b30da672e3ace64
Jarmo Cluyse <jarmo.cluyse@movu-robotics.com>
2025-02-15 22:15:21 +0100

    Revert "feat: updated comment to mini.comment"
    This reverts commit 142682042cdccd3db0854ac1c188f6aea812062a.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -21,8 +21,6 @@ return {
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] []
       require("mini.surround").setup()
-
-      require("mini.comment").setup()
     end,
   },
 }
