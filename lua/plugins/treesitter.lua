c6475dbb681897894d8e3a8fc089ece4b6dbea7d
AnaCondaa_12 <jarmo_cluyse@hotmail.com>
2026-02-01 20:12:43 +0100

    fix: revert treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = "master",
     main = "nvim-treesitter.configs", -- Sets main module to use for opts
     opts = {
       ensure_installed = {
