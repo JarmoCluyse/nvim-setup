8f7aa18a2328ba01914e26853cc651578b50a8a2
AnaCondaa_12 <jarmo_cluyse@hotmail.com>
2026-06-24 08:21:35 +0200

    feat: added neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+          },
+        },
+      })
+      vim.keymap.set("n", "<leader>e", "<CMD>Neotree toggle<CR>", { desc = "Toggle file explorer" })
+    end,
+  },
+}
